040394b647aa4ef540e2116c440eb857b887bada
meachsenbroseth <meachsenbroseth1@gmail.com>
2026-03-08 16:20:18 +0700

    API Register with google

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/google/login', [GoogleLoginController::class, 'login']);
