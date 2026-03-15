394f8bf8f3cb232b95b333810faa40aea662ba29
Sengly Sea <optionalsa@gmail.com>
2026-03-15 16:12:52 +0700

    API CRUD Road Map

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\OtpController;
+use App\Http\Controllers\RoadMapController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [OtpController::class, 'register']);
 Route::post('/otp/verify', [OtpController::class, 'verifyOtp']);
 Route::post('/otp/resend', [OtpController::class, 'resendOtp']);
+
+Route::apiResource('roadmaps', RoadMapController::class);
