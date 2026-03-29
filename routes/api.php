ef031dd74abd42e9e5ff4b6662381b63635cf0ed
NalenSrin123 <srinnalen@gmail.com>
2026-03-29 14:21:58 +0700

    Merge

--- routes/api.php
+++ routes/api.php
@@ -30,34 +30,32 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/send-otp', [AuthController::class, 'sendOtp']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
     Route::get('/courses', [CourseController::class, 'index']);
     Route::post('/courses', [CourseController::class, 'store']);
     Route::get('/courses/{id}', [CourseController::class, 'show']);
     Route::put('/courses/{id}', [CourseController::class, 'update']);
     Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->group(function () {
+// });
+// Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile-schools', [ProfileSchoolController::class, 'index']);
     Route::post('/profile-schools', [ProfileSchoolController::class, 'store']);
     Route::get('/profile-schools/{id}', [ProfileSchoolController::class, 'show']);
     Route::put('/profile-schools/{id}', [ProfileSchoolController::class, 'update']);
     Route::delete('/profile-schools/{id}', [ProfileSchoolController::class, 'destroy']);
-});
+// });
 
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     //user crud
     Route::get('/users', [UserController::class, 'index'])->name('user.index');
     Route::post('/users', [UserController::class, 'store'])->name('user.store');
     Route::get('/users/{id}', [UserController::class, 'show'])->name('user.show');
     Route::put('/users/{id}', [UserController::class, 'update'])->name('user.update');
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-
-    return $request->user();
-});
+// });
 
 
 Route::post('/register', [OtpController::class, 'register']);
